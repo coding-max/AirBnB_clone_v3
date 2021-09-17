fbd154edff6c012d1cf5164e337e50c1ee1184fc
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-17 17:59:27 +0000

    301#6 updated errorhandler

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,7 @@ def flask_isnt_fun(chester):
 @app.errorhandler(404)
 def page_not_found(err):
     """handles page not found"""
-    return make_response(jsonify({"error": "Not Found"}), 404)
+    return make_response(jsonify(error="Not found"), 404)
 
 
 if __name__ == "__main__":
