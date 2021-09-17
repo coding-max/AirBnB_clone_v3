25bb1593c35de0076a14014594eb7815f3a839f4
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-17 17:44:12 -0300

    updated files to comply requirements & added view for Review class (#2)
    * 301#3 updated DBStorage and FileStorage (added new methods)
    
    * 301#4 updated file (pep8)
    
    * 301#4 updated file (pep8)
    
    * 301#4 modified file (execute permission)
    
    * 301#6 updated errorhandler
    
    * 301#4 updated file (pep8)
    
    * 301#12 added file
    
    * updated file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-''' app module '''
+"""app module"""
 
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.teardown_appcontext
 def flask_isnt_fun(chester):
-    ''' handles teardown_appcontext '''
+    """handles teardown_appcontext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(err):
-    ''' handles page not found '''
-    return make_response(jsonify({"error": "Not Found"}), 404)
+    """handles page not found"""
+    return make_response(jsonify(error="Not found"), 404)
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST")
