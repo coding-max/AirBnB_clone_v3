59feb58a1ff4c0fee47fdd98c7c4accc12e27319
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-17 22:59:03 +0000

    places reviews fix

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -43,7 +43,7 @@ def delete_review_by_id(review_id):
         abort(404)
     review.delete()
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/places/<string:place_id>/reviews', methods=['POST'],
